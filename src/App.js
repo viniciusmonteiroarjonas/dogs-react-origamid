2c46efbc3f23992cd04732fd316acc65d81f552d
vinicius monteiro <vma.developer@gmail.com>
2021-04-02 19:23:21 -0300

    :lipstick: estrutura inicial do site

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import './App.css';
+import Header from './templates/site/Header';
+import Footer from './templates/site/Footer';
+import Home from './pages/Home';
+import Login from './pages/Login';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 function App() {
-  return <div>App React</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
